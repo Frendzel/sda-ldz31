5d946e7f6a00b1366527641d7e9da5ac6a568fcd
Marcin <marcinerbel90@gmail.com>
2022-01-22 11:13:57 +0100

    Workshop 2 - in progress * JokeTest + Gson

--- src/test/java/pl/sda/chuck/dto/JokeTest.java
+++ src/test/java/pl/sda/chuck/dto/JokeTest.java
@@ -1,13 +1,22 @@
 package pl.sda.chuck.dto;
 
+import com.google.gson.Gson;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class JokeTest {
 
+    //given
+    Gson gson = new Gson();
+
     @Test
+    @DisplayName("Serialization test for Joke dto - positive")
     public void pojoToJsonTest() {
+        //given
         Joke joke = Joke.builder()
                 .type("SUCCESS")
                 .value(JokeValue.builder()
@@ -17,7 +26,12 @@ class JokeTest {
                         .build())
                 .build();
 
-        System.out.println(joke);
+        //when
+        String result = gson.toJson(joke);
+
+        //then
+        assertTrue(result.contains("\"type\":\"SUCCESS\""));
+        assertTrue(result.contains("\"value\":{\"id\":1,\"joke\":\"Very funny joke\",\"categories\":[\"nerdy\"]}"));
     }
 
 }
