f16e8d3c34715de59e24e143b06afda50f928c17
Marcin <marcinerbel90@gmail.com>
2022-01-29 15:37:53 +0100

    Workshop 4 - in progress joke form

--- src/main/java/pl/sda/chuck/controller/JokeMvcController.java
+++ src/main/java/pl/sda/chuck/controller/JokeMvcController.java
@@ -1,18 +1,26 @@
 package pl.sda.chuck.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import pl.sda.chuck.dao.BaseResponse;
+import pl.sda.chuck.dao.RESPONSE_STATUS;
 import pl.sda.chuck.dto.Joke;
 import pl.sda.chuck.service.JokeService;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
 
 @Controller
+@Slf4j
 public class JokeMvcController {
 
     @Autowired
@@ -43,5 +51,17 @@ public class JokeMvcController {
 
         model.addAttribute("jokes", jokes);
         return "joke-list"; // html name, should match
+    }
+
+    @PostMapping("/joke/view/add")
+    public BaseResponse saveJoke(@Valid @RequestBody Joke joke, final BindingResult bindingResult) {
+        log.info("Joke to be saved: {}", joke);
+        jokeService.save(joke);
+        return new BaseResponse(RESPONSE_STATUS.SUCCESS);
+    }
+
+    @GetMapping("/view/form")
+    public String formJoke(Joke joke){
+        return "joke-form";
     }
 }
