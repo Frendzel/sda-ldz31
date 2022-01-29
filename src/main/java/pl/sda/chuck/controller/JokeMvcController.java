f93efc1381595b7c9e6ea91cc4f7d7b5ee0c4e46
Marcin <marcinerbel90@gmail.com>
2022-01-29 14:54:37 +0100

    Workshop 4 - in progress HTML + joke list

--- src/main/java/pl/sda/chuck/controller/JokeMvcController.java
+++ src/main/java/pl/sda/chuck/controller/JokeMvcController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import pl.sda.chuck.dto.Joke;
 import pl.sda.chuck.service.JokeService;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Controller
 public class JokeMvcController {
 
@@ -18,5 +23,24 @@ public class JokeMvcController {
         Joke joke = jokeService.getRandomJoke().orElseThrow();
         model.addAttribute("joke", joke); // object joke put in map on key joke
         return "joke"; // html name, should match
+    }
+
+    @GetMapping("/view/jokes")
+    public String showJokes(Model model) {
+
+        //for
+//        List<Joke> jokes = new ArrayList<>();
+//        for (int i=0; i<10; i++){
+//            jokes.add(jokeService.getRandomJoke().orElseThrow());
+//        }
+
+        //IntStream
+        List<Joke> jokes = IntStream.range(0, 10)
+                .boxed()
+                .map(integer -> jokeService.getRandomJoke().orElseThrow()).toList();
+
+
+        model.addAttribute("jokes", jokes);
+        return "joke-list"; // html name, should match
     }
 }
