0bea755d4de36d574effe4f3f685c93d10e2a43a
Marcin <marcinerbel90@gmail.com>
2022-01-29 12:46:22 +0100

    Workshop 4 - in progress JMS Implementation

--- src/main/java/pl/sda/chuck/jms/JokeProducer.java
+++ src/main/java/pl/sda/chuck/jms/JokeProducer.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
+import pl.sda.chuck.dto.Joke;
+import pl.sda.chuck.dto.JokeValue;
 
 import javax.annotation.PostConstruct;
 
@@ -14,9 +16,18 @@ public class JokeProducer {
     @Autowired
     JmsTemplate jmsTemplate;
 
+    //TODO MessageConverter
+
     @PostConstruct
     public void sendMessage() {
-        jmsTemplate.convertAndSend("messageBox", "Hello World");
+        jmsTemplate.convertAndSend(
+                "messageBox",
+                Joke.builder().type("SUCCESS").value(
+                        JokeValue.builder()
+                                .id(1)
+                                .joke("Funny Joke")
+                                .build()).build()
+        );
         log.info("Joke has been sent!");
     }
 }
