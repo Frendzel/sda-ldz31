bb8041eea74ee6e1789a25d8e76d292cc2f4b642
Marcin <marcinerbel90@gmail.com>
2022-02-05 23:51:35 +0100

    Workshop 5 - in progress * MongoDB setup password

--- src/main/java/pl/sda/chuck/mongo/MongoConfiguration.java
+++ src/main/java/pl/sda/chuck/mongo/MongoConfiguration.java
@@ -2,18 +2,22 @@ package pl.sda.chuck.mongo;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration //https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/#reference
 public class MongoConfiguration {
 
+    @Value("${mongo.password}")
+    private String password;
+
     /*
      * Use the standard Mongo driver API to create a com.mongodb.client.MongoClient instance.
      */
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb+srv://<user>:<password>@cluster0.xlisg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        return MongoClients.create("mongodb+srv://user:" + password + "@cluster0.xlisg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
     }
 
 }
