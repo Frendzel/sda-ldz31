1571756b0c443647f683bda8eb42213d0e317f73
Marcin <marcinerbel90@gmail.com>
2022-02-05 15:35:36 +0100

    Workshop 5 - in progress CSVPrinter

--- src/main/java/pl/sda/chuck/reports/CsvGenerator.java
+++ src/main/java/pl/sda/chuck/reports/CsvGenerator.java
@@ -1,12 +1,24 @@
 package pl.sda.chuck.reports;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import pl.sda.chuck.dao.JokeEntity;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 public class CsvGenerator implements Generator {
     @Override
     public void generate(List<JokeEntity> jokes) {
+        //try-with-resources construction to close csvPrinter
+        try (FileWriter fileWriter = new FileWriter("jokes.csv");
+             CSVPrinter csvPrinter = new CSVPrinter(fileWriter, CSVFormat.DEFAULT)) {
 
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
 }
