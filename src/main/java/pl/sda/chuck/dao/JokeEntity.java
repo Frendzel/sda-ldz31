086dd90ce92bd79aae755a76e0fd1ba0a3efbfe3
Marcin <marcinerbel90@gmail.com>
2022-02-05 13:24:42 +0100

    Workshop 5 - in progress H2 Support

--- src/main/java/pl/sda/chuck/dao/JokeEntity.java
+++ src/main/java/pl/sda/chuck/dao/JokeEntity.java
@@ -4,16 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class JokeEntity {
+    @Id
+    @GeneratedValue
     Integer id;
     Integer externalId;
     String joke;
     //TODO should be then moved to dedicated entity with many-to-many relationship
+    @ElementCollection
     List<String> categories;
 
     public Integer getId() {
