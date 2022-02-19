a48dbb3888340b906b9a1aa448f5b74f67c22901
Marcin <marcinerbel90@gmail.com>
2022-02-19 13:00:34 +0100

    Workshops - advanced testing positive mapping test

--- src/test/java/pl/sda/chuck/mapper/JokeManualMapperTest.java
+++ src/test/java/pl/sda/chuck/mapper/JokeManualMapperTest.java
@@ -1,11 +1,13 @@
 package pl.sda.chuck.mapper;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pl.sda.chuck.dao.JokeEntity;
 import pl.sda.chuck.dto.Joke;
 import pl.sda.chuck.dto.JokeValue;
+import pl.sda.chuck.exception.ExternalTechnicalException;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,32 +34,51 @@ class JokeManualMapperTest {
         //when -> map method
         JokeEntity result = map(testedObject);
         //then -> assertion
-        assertEquals(result.getJoke(), testedObject.getValue().getJoke());
-        assertEquals(result.getCategories(), testedObject.getValue().getCategories());
+        assertEquals(testedObject.getValue().getJoke(), result.getJoke());
+        assertEquals(testedObject.getValue().getCategories(), result.getCategories());
+        assertEquals(1, result.getExternalId());
+        assertEquals(null, result.getId());
     }
 
     @Test
-        //consider name of method
-    void testMapPositive() {
+    @DisplayName("Testing mapping between Optional DTO and Entity using ManualMapper - Positive")
+    void mapOptionalDtoToEntityPositiveTest() {
         //given -> object to compare
+        Joke testedObject = builder()
+                .value(JokeValue
+                        .builder()
+                        .joke("Very funny joke")
+                        .id(1)
+                        .categories(List.of("nerdy"))
+                        .build())
+                .type("SUCCESS")
+                .build();
         //when -> map method
-        JokeEntity result = map(Optional.of(Joke.builder().value(JokeValue.builder().build()).build()));
+        JokeEntity result = map(Optional.of(testedObject));
         //then -> assertion
         Assertions.assertNotNull(result);
+        assertEquals(testedObject.getValue().getJoke(), result.getJoke());
+        assertEquals(testedObject.getValue().getCategories(), result.getCategories());
+        assertEquals(1, result.getExternalId());
+        assertEquals(null, result.getId());
     }
 
     @Test
-        //consider name of method
+    @DisplayName("Testing mapping between Optional DTO and Entity using ManualMapper - Negative")
     void testMapNegative() {
-        //given -> object to compare
-        //when -> map method
-        JokeEntity result = map(Optional.of(Joke.builder().build()));
-        //then -> assertion
-        Assertions.assertNotNull(result);
+//        try {
+//            JokeEntity result = map(Optional.of(Joke.builder().build()));
+//            fail();
+//        } catch (Exception e) {
+//            Assertions.assertNotNull(e);
+//        }
+        Assertions.assertThrows(ExternalTechnicalException.class, () -> map(Optional.of(builder().build())));
     }
 
     @Test
-        //consider name of method
+    //consider name of method
+    @Disabled
+        // junit 4 -> @Ignore
     void mapDtoToEntityNegative() {
         //given -> object to compare
         //when -> map method
